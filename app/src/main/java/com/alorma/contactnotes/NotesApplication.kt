9c8a00c6b684773f786c1993e0fab6b503ec56ca
Bernat Borrás <bernatbor15@gmail.com>
2017-10-13 14:42:13 +0200

    Update gradle

--- app/src/main/java/com/alorma/contactnotes/NotesApplication.kt
+++ app/src/main/java/com/alorma/contactnotes/NotesApplication.kt
@@ -1,13 +1,12 @@
 package com.alorma.contactnotes
 
 import android.app.Application
-import com.github.tamir7.contacts.Contacts
+import android.support.multidex.MultiDexApplication
 import com.google.firebase.FirebaseApp
 
-class NotesApplication: Application() {
+class NotesApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
-        Contacts.initialize(this);
     }
 }
--- app/src/main/java/com/alorma/contactnotes/data/contacts/AndroidContactsDataSource.kt
+++ app/src/main/java/com/alorma/contactnotes/data/contacts/AndroidContactsDataSource.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.provider.ContactsContract
 import com.alorma.contactnotes.domain.contacts.Contact
 import com.alorma.contactnotes.domain.create.CreateUserForm
-import com.github.tamir7.contacts.Contacts
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Maybe
@@ -18,50 +17,11 @@ class AndroidContactsDataSource(private val contentResolver: ContentResolver) : 
     }
 
     override fun getContacts(): Flowable<List<Contact>> {
-        return Flowable.fromCallable({
-            loadContacts()
-        })
-    }
-
-    private fun loadContacts(): List<Contact> = Contacts.getQuery().hasPhoneNumber().find().map {
-        mapContact(it)
+        return Flowable.empty()
     }
 
     override fun getContactByRawId(rawId: String): Maybe<Contact> {
-        return Maybe.fromCallable {
-            val queryId = Contacts.getQuery().whereEqualTo(com.github.tamir7.contacts.Contact.Field.ContactId, rawId)
-            val queryPhone = Contacts.getQuery().whereEqualTo(com.github.tamir7.contacts.Contact.Field.PhoneNumber, deNormalizeString(rawId))
-            val queryPhoneNormalized = Contacts.getQuery().whereEqualTo(com.github.tamir7.contacts.Contact.Field.PhoneNormalizedNumber, deNormalizeString(rawId))
-            Contacts.getQuery().or(listOf(queryId, queryPhone, queryPhoneNormalized)).find()
-        }.filter { it.isNotEmpty() }
-                .map { it[0] }
-                .map { mapContact(it) }
-    }
-
-    private fun mapContact(contact: com.github.tamir7.contacts.Contact): Contact {
-        val id: String = normalizeString(getPhoneNumber(contact))
-        return Contact(id, contact.displayName, contact.photoUri)
-    }
-
-    private fun normalizeString(string: String): String {
-        return string.toLowerCase().replace(" ", "-")
-    }
-
-    private fun deNormalizeString(string: String): String {
-        return string.toLowerCase().replace("-", " ")
-    }
-
-    private fun getPhoneNumber(contact: com.github.tamir7.contacts.Contact): String {
-        val phoneNumber = contact.phoneNumbers
-                .filterNot { phoneNumber -> phoneNumber == null }
-                .sortedBy { it.type }
-                .sortedBy { it.label }[0]
-
-        return if (phoneNumber.normalizedNumber != null) {
-            phoneNumber.normalizedNumber
-        } else {
-            phoneNumber.number
-        }
+        return Maybe.never()
     }
 
     override fun insertContact(createUserForm: CreateUserForm): Completable = Completable.never()
@@ -96,7 +56,7 @@ class AndroidContactsDataSource(private val contentResolver: ContentResolver) : 
 
     override fun loadContactByLookup(lookup: String): Single<Contact> {
         return Single.defer {
-            val uri = Uri.parse("content://com.android.contacts/contacts/lookup/$lookup")
+            val uri = Uri.parse("$LOOKUP_URI$lookup")
             loadContact(uri.toString())
         }
     }
