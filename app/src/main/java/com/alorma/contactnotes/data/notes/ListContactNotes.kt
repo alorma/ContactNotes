4c5470c7d9edb68fd5a479eb374efcefa3c43e02
Bernat Borrás <bernatbor15@gmail.com>
2017-10-24 15:39:51 +0200

    WIP

--- app/src/main/java/com/alorma/contactnotes/data/notes/ListContactNotes.kt
+++ app/src/main/java/com/alorma/contactnotes/data/notes/ListContactNotes.kt
@@ -4,6 +4,6 @@ import com.alorma.contactnotes.domain.notes.Note
 
 class ListContactNotes(private val contactsNotesProvider: ContactsNotesProvider) {
     fun list(userId: String): List<Note> {
-        return contactsNotesProvider.list(userId)
+        return contactsNotesProvider.list(userId).sortedByDescending { it.date }
     }
 }
