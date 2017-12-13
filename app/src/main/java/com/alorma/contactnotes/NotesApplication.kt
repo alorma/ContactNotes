b67b7deb37eec8aad09679edf3890ad0639ac346
Bernat Borrás <bernatbor15@gmail.com>
2017-12-13 09:27:46 +0100

    Add todo

--- app/src/main/java/com/alorma/contactnotes/NotesApplication.kt
+++ app/src/main/java/com/alorma/contactnotes/NotesApplication.kt
@@ -7,6 +7,10 @@ import com.alorma.contactnotes.data.contacts.store.ContactsListProvider
 import com.alorma.contactnotes.data.notes.store.NotesProvider
 import com.google.firebase.FirebaseApp
 
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
 class NotesApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
